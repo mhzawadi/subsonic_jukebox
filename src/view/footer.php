74680e14f44c6b6f62f02a8c8149c33ce14557d9
Matthew Horwood <matt@horwood.biz>
2019-07-22 22:17:50 +0100

    add scroll to top button

--- src/view/footer.php
+++ src/view/footer.php
@@ -1,4 +1,25 @@
 <?php
 
-$footer = '  </body>
+$footer = '  <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+</body>
+<script>
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+
+     $(\'html, body\').animate({scrollTop:0}, \'slow\');
+}
+</script>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </html>';
--- src/view/header.php
+++ src/view/header.php
@@ -127,6 +127,25 @@ $header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
         background-color: #555555;
         color: white;
     }
+    #myBtn {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  background-color: red;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+}
+
+#myBtn:hover {
+  background-color: #555;
+}
 </style>
 </head>
 

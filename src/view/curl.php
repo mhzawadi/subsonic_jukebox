7a09f15388570b1366c7313d9679d3547c0ede22
Matthew Horwood <matt@horwood.biz>
2019-08-04 10:20:46 +0100

    add if playing

--- src/view/curl.php
+++ src/view/curl.php
@@ -1,9 +1,12 @@
 <?php
-
-$song_list = $songs['jukeboxPlaylist']['entry'];
-foreach($song_list as $key => $song){
-  if("$key" === "$currentIndex"){
-    $html .= 'Playing: '.$song['@attributes']['title'].' From '.
-        $song['@attributes']['album'].' By '.$song['@attributes']['artist'];
+if($playing === true){
+  $song_list = $songs['jukeboxPlaylist']['entry'];
+  foreach($song_list as $key => $song){
+    if("$key" === "$currentIndex"){
+      $html .= 'Playing: '.$song['@attributes']['title'].' From '.
+          $song['@attributes']['album'].' By '.$song['@attributes']['artist'];
+    }
   }
+}else{
+  $html .= 'not playing right now';
 }

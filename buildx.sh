4f80036e69b4cf4ee5edc1019b55f64c8dfa41d0
Matthew Horwood <matt@horwood.biz>
2020-04-17 21:47:24 +0100

    feat: buildx tooling

---
+++ buildx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -lh 1]
+then
+  echo "this needs a tag please"
+  exit 1
+else
+  TAG=$1
+fi
+
+git pull;
+git checkout "${TAG}"
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
+       -t mhzawadi/subsonic_jukebox:${TAG} --push .
